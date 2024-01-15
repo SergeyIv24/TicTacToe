b08c57f603008083ed0e28c7417f056d73d2d91b
Sergey Ivanov <Reger21a@yandex.ru>
2024-01-15 16:51:53 +0300

    Возможность выбора игры против ПК или другого человека

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,2 +1,39 @@
+import java.util.Scanner;
+
 public class GameManager {
+
+
+    public static void chooseGameOption() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Это крестики нолики!");
+        System.out.println("1 - Игра против другого игрока, 2 - игра против компьютера");
+        byte userOption = scanner.nextByte();
+
+        switch (userOption) {
+            case 1:
+                GamerVsGamer game = new GamerVsGamer();
+                game.game();
+                break;
+            case 2:
+                System.out.println("1 - легко. 2 - сложно.");
+                byte levelSelection = scanner.nextByte();
+                switch (levelSelection) {
+                    case 1:
+                        GamerVsComputer gamerVsComputer = new GamerVsComputer();
+                        gamerVsComputer.gameAgainstComputer();
+                        break;
+                    case 2:
+                        System.out.println("Опция пока недоступна");
+                        break;
+                    default:
+                        System.out.println("Такой опции нет");
+                        break;
+                }
+                break;
+            default:
+                System.out.println("Такой опции нет");
+        }
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        GamerVsGamer game = new GamerVsGamer();
-        //game.game();
-        GamerVsComputer gamerVsComputer = new GamerVsComputer();
-        gamerVsComputer.gameAgainstComputer();
+        GameManager.chooseGameOption();
 
     }
 }
